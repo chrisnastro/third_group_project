4ff1ad789c1be2ecdbb491fa284715b146035d64
chrisnastro <cnastro@icloud.com>
2024-04-02 23:22:37 -0400

    updated font with google fonts, changed background colors, and some text properties

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -11,13 +11,13 @@ const Header = () => {
     <header className="bg-info text-dark mb-4 py-3 display-flex align-center">
       <div className="container flex-column justify-space-between-lg justify-center align-center text-center">
         <Link className="text-dark" to="/">
-          <h1 className="m-0" style={{ fontSize: '3rem' }}>
-            Tech Friends
+          <h1 className="m-0" style={{ fontSize: '4rem' }}>
+            Employee Network
           </h1>
         </Link>
-        <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
+        {/* <p className="m-0" style={{ fontSize: '1.75rem', fontWeight: '700' }}>
           Meet your new programming pals.
-        </p>
+        </p> */}
         <div>
           {Auth.loggedIn() ? (
             <>
