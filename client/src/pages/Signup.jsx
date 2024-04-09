3ac51c83ddd0d07b6b1b0d8322a9bf32392df86b
e6m9 <alostsound@gmail.com>
2024-04-08 21:15:45 -0400

    signup dropdown functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 import { useMutation } from '@apollo/client';
 import { ADD_PROFILE } from '../utils/mutations';
 import Auth from '../utils/auth';
-import Controller from 
+
+import SignupDropdown from '../components/SignupDropdown';
+import PublicProfileList from '../components/PublicProfileList';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -13,129 +16,102 @@ const Signup = () => {
     email: '',
     password: '',
   });
-  // Added state for department ID
-  const [dept_id, setDeptId] = useState('');
 
-  const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
+  const [signupSuccess, setSignupSuccess] = useState(false);
+  const [titles, setTitles] = useState([]);
+  const [departments, setDepartments] = useState([]);
 
-  // Update formState on input change
+  const [addProfile, { error }] = useMutation(ADD_PROFILE);
+
+  // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormState({ ...formState, [name]: value });
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   };
 
-  // Options for department dropdown
-  const options = [
-    { value: "Marketing", label: "Marketing" },
-    { value: "Sales", label: "Sales" },
-    { value: "Human Resources", label: "Human Resources" },
-  ];
-
-  // Handler for department selection
-  const changeHandler = (event) => {
-    setDeptId(event.target.value);
-    // Also update formState.department if necessary
-    setFormState({ ...formState, department: event.target.value });
-  };
-
-  // Submit form
+  // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(formState);
 
     try {
       const { data } = await addProfile({
         variables: { ...formState },
       });
 
-      Auth.login(data.addProfile.token);
+      if (data.addProfile.token) {
+        Auth.login(data.addProfile.token); 
+        setSignupSuccess(true);
+      }
     } catch (e) {
-      console.log()
       console.error(e);
     }
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your username"
-                  name="name"
-                  type="text"
-                  value={formState.name}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="Your title"
-                  name="title"
-                  type="text"
-                  value={formState.title}
-                  onChange={handleChange}
-                />
-                <Controller
-                  name="department"
-                  control={control}
-                  rules={{ required: 'Department is required.' }}
-                  render={({ field, fieldState }) => (
-                    <Dropdown
-                      id={field.department}
-                      value={field.value}
-                      optionLabel="name"
-                      placeholder="Select a Department"
-                      options={options}
-                      focusInputRef={field.ref}
-                      onChange={(e) => field.onChange(e.value)}
-                      className={classNames({ 'p-invalid': fieldState.error })}
-                    />
-                  )}
-                />
+    <>
+      <PublicProfileList setTitles={setTitles} setDepartments={setDepartments} />
+      <main className="flex-row justify-center mb-4">
+        <div className="col-12 col-lg-10">
+          <div className="card">
+            <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
+            <div className="card-body">
+              {signupSuccess ? (
+                <p>
+                  Success! You may now head{' '}
+                  <Link to="/">back to the homepage.</Link>
+                </p>
+              ) : (
+                <form onSubmit={handleFormSubmit}>
+                  <input
+                    className="form-input"
+                    placeholder="Your username"
+                    name="name"
+                    type="text"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className="form-input"
+                    placeholder="Your email"
+                    name="email"
+                    type="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                  <SignupDropdown label="Title__" options={titles} value={formState.title} onChange={handleChange} />
+                  <SignupDropdown label="Department__" options={departments} value={formState.department} onChange={handleChange} />
+                  <input
+                    className="form-input"
+                    placeholder="******"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
+                  <button
+                    className="btn btn-block btn-info"
+                    style={{ cursor: 'pointer' }}
+                    type="submit"
+                  >
+                    Submit
+                  </button>
+                </form>
+              )}
 
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-info"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
+              {error && (
+                <div className="my-3 p-3 bg-danger text-white">
+                  {error.message}
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main >
+    </>
   );
 };
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,8 +12,6 @@ export const ADD_PROFILE = gql`
   }
 `;
 
-
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
